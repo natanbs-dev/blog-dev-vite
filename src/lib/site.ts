5820a0b4af6d8602bd6b7c8684ff6d9d0ee390f6
barbosa-arch <natanbarbosa50@gmail.com>
2026-09-19 23:57:47 -0300

    fix email

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -2,7 +2,7 @@ export const site = {
   name: 'barbosa.md',
   author: 'Natan Barbosa',
   url: 'http://localhost:5173',
-  email: 'hello@barbosa.md',
+  email: 'natan_nbs@protonmail.com',
   description:
     'Blog de programação: guias práticos, boas práticas e reflexões sobre engenharia de software, ferramentas e produtividade.',
   github: 'https://github.com/',
